93b2483e92ca5fc64fe11f8eccb2858b96858000
RahulGorai0206 <rahulgorai0206@gmail.com>
2022-08-26 22:04:51 +0530

    DSA: updating the Kadane's algorithm code

--- MaxSubarraySum_KadaneAlgo.java
+++ MaxSubarraySum_KadaneAlgo.java
@@ -1,15 +1,13 @@
-// print max sum of all sub-array sums using kadane's alsorithm
+// print max sum of all sub-array sums using Kadane's alsorithm
 public class MaxSubarraySum_KadaneAlgo {
     public static int sum(int arr[]) {
         int sum = 0, max = Integer.MIN_VALUE;
-        for (int i = 0; i < arr.length; i++) {      // loop for calculating the sum
-            sum += arr[i];          // adding sum
-            if (sum < 0) {          // applying kadane's algo which is "if your sum is negetive then set it to zero which is better"
+        for (int i = 0; i < arr.length; i++) { // loop for calculating the sum
+            sum += arr[i]; // adding sum
+            if (sum < 0) { // applying Kadane's algo which is "if your sum is negetive then set it to zero which is better"
                 sum = 0;
             }
-            if (sum > max) {
-                max = sum;          // comparing the current with current max value
-            }
+            max = Math.max(sum, max); // comparing the current with current max value via Math function
         }
         return max;
     }
@@ -19,7 +17,6 @@ public class MaxSubarraySum_KadaneAlgo {
         System.out.println("Max Value is: " + sum(arr));
     }
 }
-
 
 // OUTPUT
 // Max Value is: 7
